dfbc80594343143fa95e53fbe23537d2f4163791
JeanPaulRF <64498696+JeanPaulRF@users.noreply.github.com>
2022-10-05 19:29:27 -0600

    Initial commit

--- djangoproject/teatronacional/teatronacional/asgi.py
+++ djangoproject/teatronacional/teatronacional/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for teatro project.
+ASGI config for teatronacional project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teatro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teatronacional.settings')
 
 application = get_asgi_application()
--- djangoproject/teatronacional/teatronacional/wsgi.py
+++ djangoproject/teatronacional/teatronacional/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for teatro project.
+WSGI config for teatronacional project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teatro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teatronacional.settings')
 
 application = get_wsgi_application()
